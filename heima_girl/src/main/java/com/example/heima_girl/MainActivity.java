300f3ccfc3b8327add159c3a1ad56ba3c167fb9a
Your Name <you@example.com>
2017-03-10 21:25:05 +0800

    发送同步请求

--- heima_girl/src/main/java/com/example/heima_girl/MainActivity.java
+++ heima_girl/src/main/java/com/example/heima_girl/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.heima_girl;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @BindView(R.id.listview)
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //绑定activity
+        ButterKnife.bind(this);
+        sendSyncRequest();
+        
+    }
+
+    private void sendSyncRequest() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                String url = "http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/1";
+                Request request = new Request.Builder().get().url(url).build();
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    Log.d(TAG, "response." + response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
