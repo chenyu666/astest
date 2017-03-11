e757ad03bea53146ebd498c757505e47c392e9e8
Your Name <you@example.com>
2017-03-11 11:04:23 +0800

    搞定

--- heima_girl/src/main/java/com/example/heima_girl/MainActivity.java
+++ heima_girl/src/main/java/com/example/heima_girl/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.heima_girl;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -18,7 +30,10 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     @BindView(R.id.listview)
-    ListView listview;
+    ListView mListview;
+    private Gson mGson = new Gson();
+    private boolean isLoading = false;
+    private List<ResultBean.ResultsBean> mListData = new ArrayList<ResultBean.ResultsBean>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +41,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //绑定activity
         ButterKnife.bind(this);
+        mListview.setAdapter(mAdapter);
+        mListview.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if (scrollState == SCROLL_STATE_IDLE) {
+                    if (mListview.getLastVisiblePosition() == mListData.size() - 1 && !isLoading) {
+                        loadMoreData();
+                    }
+                }
+            }
+
+
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
         //sendSyncRequest();
         sendAsyncRequest();
     }
+
+    private void loadMoreData() {
+        //异步请求 ,在子线程执行
+        isLoading = true;
+        OkHttpClient okHttpClient = new OkHttpClient();
+        int nextIndex = mListData.size() / 10 + 1;
+        String url = "http://gank.io/api/data/福利/10/" + nextIndex;
+        Request request = new Request.Builder().get().url(url).build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String result = response.body().string();
+                ResultBean resultBean = mGson.fromJson(result, ResultBean.class);
+                mListData.addAll(resultBean.getResults());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+                isLoading = false;
+                //Log.d(TAG, "onResponse: " + resultBean.getResults().get(0).getUrl());
+
+            }
+        });
+    }
+
 
     private void sendAsyncRequest() {
         //异步请求 ,在子线程执行
@@ -44,7 +107,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 String result = response.body().string();
-                Log.d(TAG, "onResponse: " + result);
+                ResultBean resultBean = mGson.fromJson(result, ResultBean.class);
+                mListData.addAll(resultBean.getResults());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+                //Log.d(TAG, "onResponse: " + resultBean.getResults().get(0).getUrl());
 
             }
         });
@@ -67,4 +138,53 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
     }
+
+    private BaseAdapter mAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return mListData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                convertView = View.inflate(MainActivity.this, R.layout.item_list, null);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            ResultBean.ResultsBean resultsBean = mListData.get(position);
+            holder.mTextView.setText(resultsBean.getPublishedAt());
+            String url = resultsBean.getUrl();
+            Glide.with(MainActivity.this).load(url).centerCrop().
+                    bitmapTransform(new CropCircleTransformation(MainActivity.this)).
+                    into(holder.mImageView);
+            return convertView;
+        }
+    };
+
+    static class ViewHolder {
+        ImageView mImageView;
+        TextView mTextView;
+
+        public ViewHolder(View root) {
+            mImageView = (ImageView) root.findViewById(R.id.iv);
+            mTextView = (TextView) root.findViewById(R.id.tv);
+
+        }
+
+    }
+
 }
